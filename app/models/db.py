3cc0c9b282e6ae7bd283b1523becc726a1ad4961
Will Torres <w.torresjr@gmail.com>
2023-12-08 23:27:56 -0500

    Added faker seed data for clients

--- app/models/db.py
+++ app/models/db.py
@@ -1,10 +1,13 @@
 from flask_sqlalchemy import SQLAlchemy
+from faker import Faker
+
 
 import os
 environment = os.getenv("FLASK_ENV")
 SCHEMA = os.environ.get("SCHEMA")
 
 
+fake = Faker()
 db = SQLAlchemy()
 
 # helper function for adding prefix to foreign key column references in production
--- migrations/versions/20231208_232425_.py
+++ migrations/versions/20231208_232425_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: ed2dc9d680b8
+Revision ID: a2337de5ef3b
 Revises: 
-Create Date: 2023-12-08 22:53:47.080610
+Create Date: 2023-12-08 23:24:25.541966
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'ed2dc9d680b8'
+revision = 'a2337de5ef3b'
 down_revision = None
 branch_labels = None
 depends_on = None
