d2d5da6ed9bdc375dd328b4cc51e59d88d2f4570
Will Torres <w.torresjr@gmail.com>
2023-12-09 18:09:39 -0500

    Added get my-clients end point

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,7 @@ from flask_login import LoginManager
 from .models import db, Therapist
 from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
+from .api.my_clients_routes import my_clients
 from .seeds import seed_commands
 from .config import Config
 
@@ -28,6 +29,7 @@ app.cli.add_command(seed_commands)
 app.config.from_object(Config)
 app.register_blueprint(user_routes, url_prefix="/api/therapists")
 app.register_blueprint(auth_routes, url_prefix="/api/auth")
+app.register_blueprint(my_clients, url_prefix="/api/my-clients/")
 db.init_app(app)
 Migrate(app, db)
 
--- app/api/my_clients_routes.py
+++ app/api/my_clients_routes.py
@@ -1,1 +1,12 @@
+from flask import Blueprint, jsonify
 from flask_login import current_user, login_user, logout_user, login_required
+from app.models import Client
+
+my_clients = Blueprint("my-clients", __name__)
+
+
+@my_clients.route("/", methods=["GET"])
+def get_clients():
+    clients = Client.query.filter_by(therapist_id=current_user.id).all()
+    client_list = [client.to_dict() for client in clients]
+    return jsonify({"Clients": client_list})
