8d176ed9e42644265e0c62e4750239807fc21741
Will Torres <w.torresjr@gmail.com>
2023-12-11 18:40:40 -0500

    Created route to get dts by client id

--- app/api/discreet_trials_routes.py
+++ app/api/discreet_trials_routes.py
@@ -8,6 +8,9 @@ from datetime import time, datetime
 discreet_trials_bp = Blueprint("my-discreet-trials", __name__)
 
 
+# Get all therapist created discreet trials
+
+
 @discreet_trials_bp.route("/", methods=["GET"])
 @login_required
 def get_all_discreet_trials():
@@ -21,3 +24,25 @@ def get_all_discreet_trials():
     therapist_dts = [dt.to_dict() for dt in found_discreet_trials]
 
     return jsonify({"Discreet_Trials": therapist_dts})
+
+
+# Get all therapist created discreet trials by client ID
+
+
+@discreet_trials_bp.route("/client/<int:client_id>", methods=["GET"])
+@login_required
+def get_dt_by_client_id(client_id):
+    found_client_dts = Discreet_Trial.query.filter_by(
+        client_id=client_id, therapist_id=current_user.id
+    ).all()
+
+    if not found_client_dts:
+        return jsonify(
+            {
+                "message": "Either client does not exist or does not belong to logged in user."
+            }
+        )
+
+    client_dts = [dt.to_dict() for dt in found_client_dts]
+
+    return jsonify({"Client_DTs": client_dts})
