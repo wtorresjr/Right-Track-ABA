54f2f9d350f1ece2c4781a174f556ef586016776
Will Torres <w.torresjr@gmail.com>
2023-12-11 18:26:42 -0500

    created route to get all therapist dts

--- app/api/discreet_trials_routes.py
+++ app/api/discreet_trials_routes.py
@@ -11,4 +11,13 @@ discreet_trials_bp = Blueprint("my-discreet-trials", __name__)
 @discreet_trials_bp.route("/", methods=["GET"])
 @login_required
 def get_all_discreet_trials():
-    pass
+    found_discreet_trials = Discreet_Trial.query.filter_by(
+        therapist_id=current_user.id
+    ).all()
+
+    if not found_discreet_trials:
+        return jsonify({"message", "No discreet trials found"}), 404
+
+    therapist_dts = [dt.to_dict() for dt in found_discreet_trials]
+
+    return jsonify({"Discreet_Trials": therapist_dts})
