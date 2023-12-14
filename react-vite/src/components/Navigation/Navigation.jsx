caf8c4ee5e771a97dc4cd26af4afa8d438497795
Will Torres <w.torresjr@gmail.com>
2023-12-14 00:22:04 -0500

    Added conditional loading of logo and profile button

--- react-vite/src/components/Navigation/Navigation.jsx
+++ react-vite/src/components/Navigation/Navigation.jsx
@@ -1,16 +1,26 @@
 import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
 import ProfileButton from "./ProfileButton";
 import "./Navigation.css";
 
 function Navigation() {
+  const sessionUser = useSelector((state) => state.session.user);
   return (
-    <ul>
-      <NavLink to="/">
-        <img className="logo-image" src="../right-track-aba-logo.png" />
-      </NavLink>
+    <>
+      {sessionUser && (
+        <ul>
+          <NavLink to="/">
+            <img
+              className="logo-image"
+              src="../right-track-aba-logo.png"
+              alt="Logo"
+            />
+          </NavLink>
 
-      <ProfileButton />
-    </ul>
+          <ProfileButton />
+        </ul>
+      )}
+    </>
   );
 }
 
