19f90d8be647f1cf30fa4a90e6d469661cf2fb06
Will Torres <w.torresjr@gmail.com>
2023-12-13 20:21:35 -0500

    Moved login page to / route

--- react-vite/src/router/index.jsx
+++ react-vite/src/router/index.jsx
@@ -1,7 +1,7 @@
-import { createBrowserRouter } from 'react-router-dom';
-import LoginFormPage from '../components/LoginFormPage';
-import SignupFormPage from '../components/SignupFormPage';
-import Layout from './Layout';
+import { createBrowserRouter } from "react-router-dom";
+import LoginFormPage from "../components/LoginFormPage";
+import SignupFormPage from "../components/SignupFormPage";
+import Layout from "./Layout";
 
 export const router = createBrowserRouter([
   {
@@ -9,10 +9,6 @@ export const router = createBrowserRouter([
     children: [
       {
         path: "/",
-        element: <h1>Welcome!</h1>,
-      },
-      {
-        path: "login",
         element: <LoginFormPage />,
       },
       {
