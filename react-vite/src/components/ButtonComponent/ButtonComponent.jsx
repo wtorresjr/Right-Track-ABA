5a2c68ddae8914cbd6139733a18872493137d78c
Will Torres <w.torresjr@gmail.com>
2023-12-15 00:45:51 -0500

    custom font awesome icons for each button

--- react-vite/src/components/ButtonComponent/ButtonComponent.jsx
+++ react-vite/src/components/ButtonComponent/ButtonComponent.jsx
@@ -1,15 +1,15 @@
 import "./button-component.css";
 
-const ButtonComponent = ({ btnText, btnStyle, btnClass }) => {
+const ButtonComponent = ({ btnText, btnStyle, btnClass, faAwesomeStyle }) => {
   return (
     <div className={btnClass} id={btnStyle}>
       <h2>{btnText}</h2>
-      <i
-        className="fa-solid fa-people-roof fa-2xl"
-        style={{ color: "#ffffff" }}
-      ></i>
+      <i className={faAwesomeStyle} style={{ color: "#ffffff" }}></i>
     </div>
   );
 };
 
 export default ButtonComponent;
+
+
+
--- react-vite/src/components/MainDisplay/MainDisplay.jsx
+++ react-vite/src/components/MainDisplay/MainDisplay.jsx
@@ -8,21 +8,25 @@ const MainDisplay = () => {
         btnText={"Manage Clients"}
         btnStyle={"manageClient"}
         btnClass={"button-comp-container btnHover"}
+        faAwesomeStyle={"fa-solid fa-people-roof fa-2xl"}
       />
       <ButtonComponent
         btnText={"View Reports"}
         btnStyle={"viewReports"}
         btnClass={"button-comp-container btnHover"}
+        faAwesomeStyle={"fa-solid fa-chart-column fa-2xl"}
       />
       <ButtonComponent
         btnText={"Daily Charts"}
         btnStyle={"dailyCharts"}
         btnClass={"button-comp-container btnHover"}
+        faAwesomeStyle={"fa-regular fa-calendar-days fa-2xl"}
       />
       <ButtonComponent
         btnText={"Discreet Trials"}
         btnStyle={"dtTrials"}
         btnClass={"button-comp-container btnHover"}
+        faAwesomeStyle={"fa-solid fa-list-check fa-2xl"}
       />
     </div>
   );
