bef801638029aff65a55de6e59f847457300e5df
Will Torres <w.torresjr@gmail.com>
2023-12-15 00:38:34 -0500

    Added font awesome icons to buttons

--- react-vite/src/components/ButtonComponent/ButtonComponent.jsx
+++ react-vite/src/components/ButtonComponent/ButtonComponent.jsx
@@ -4,6 +4,10 @@ const ButtonComponent = ({ btnText, btnStyle, btnClass }) => {
   return (
     <div className={btnClass} id={btnStyle}>
       <h2>{btnText}</h2>
+      <i
+        className="fa-solid fa-people-roof fa-2xl"
+        style={{ color: "#ffffff" }}
+      ></i>
     </div>
   );
 };
