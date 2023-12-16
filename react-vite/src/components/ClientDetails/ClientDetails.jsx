5fc44124406bff18af89da31d91f69e465679721
Will Torres <w.torresjr@gmail.com>
2023-12-16 16:46:25 -0500

    Additional styling to client details

--- react-vite/src/components/ClientDetails/ClientDetails.jsx
+++ react-vite/src/components/ClientDetails/ClientDetails.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { getClientByIDThunk } from "../../redux/clients";
+import "./client-details.css";
 
 const ClientDetails = () => {
   const dispatch = useDispatch();
@@ -15,10 +16,42 @@ const ClientDetails = () => {
   }, [dispatch]);
 
   return (
-    <div className="mainDisplayContain">
+    <div className="mainDisplayContain" id="clientDetails">
       <h1>
         {client?.last_name}, {client?.first_name}
       </h1>
+      <div className="clientDetailsContain">
+        <div>
+          <label>Guardian Email:</label>
+          {client?.guardian_email}
+        </div>
+        <div>
+          <label>DOB:</label>
+          {client?.dob}
+        </div>
+        <div>
+          <label>Notes:</label>
+          {client?.client_notes}
+        </div>
+      </div>
+      <div className="btnsContain">
+        <button
+          id="editBtn"
+          onClick={() => {
+            handleClick(client.id, "edit");
+          }}
+        >
+          Edit
+        </button>
+        <button
+          id="delBtn"
+          onClick={() => {
+            handleClick(client.id, "delete");
+          }}
+        >
+          Delete
+        </button>
+      </div>
     </div>
   );
 };
