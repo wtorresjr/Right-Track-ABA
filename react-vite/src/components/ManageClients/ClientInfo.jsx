2b0655ceb5a49799f931cec1b043165f8b70217e
Will Torres <w.torresjr@gmail.com>
2023-12-16 00:09:25 -0500

    added edit and delete buttons

--- react-vite/src/components/ManageClients/ClientInfo.jsx
+++ react-vite/src/components/ManageClients/ClientInfo.jsx
@@ -1,10 +1,16 @@
 import "./manage-clients.css";
 const ClientInfo = ({ client }) => {
   return (
-    <div className="clientInfoContain" key={client?.id}>
-      <div>First Name: {client?.first_name}</div>
-      <div>Last Name: {client?.last_name}</div>
-      <div>Guardian Email: {client?.guardian_email}</div>
+    <div className="clientInfoContain">
+      <div className="clientData" key={client?.id}>
+        <div>First Name: {client?.first_name}</div>
+        <div>Last Name: {client?.last_name}</div>
+        <div>Guardian Email: {client?.guardian_email}</div>
+      </div>
+      <div className="btnsContain">
+        <button>Edit</button>
+        <button>Delete</button>
+      </div>
     </div>
   );
 };
