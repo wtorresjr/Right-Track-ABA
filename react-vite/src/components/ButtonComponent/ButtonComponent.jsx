437ea108c7e59d89c4b6e63947e482b9e5728178
Will Torres <w.torresjr@gmail.com>
2023-12-15 20:01:46 -0500

    New menu system

--- react-vite/src/components/ButtonComponent/ButtonComponent.jsx
+++ react-vite/src/components/ButtonComponent/ButtonComponent.jsx
@@ -1,8 +1,16 @@
+import { useNavigate, Navigate } from "react-router-dom";
 import "./button-component.css";
 
-const ButtonComponent = ({ btnText, btnStyle, btnClass, faAwesomeStyle }) => {
+const ButtonComponent = ({
+  btnText,
+  btnStyle,
+  btnClass,
+  faAwesomeStyle,
+  urlProp,
+}) => {
+  const navigate = useNavigate();
   return (
-    <div className={btnClass} id={btnStyle}>
+    <div className={btnClass} id={btnStyle} onClick={() => navigate(urlProp)}>
       <h2>{btnText}</h2>
       <i className={faAwesomeStyle} style={{ color: "#ffffff" }}></i>
     </div>
@@ -10,6 +18,3 @@ const ButtonComponent = ({ btnText, btnStyle, btnClass, faAwesomeStyle }) => {
 };
 
 export default ButtonComponent;
-
-
-
