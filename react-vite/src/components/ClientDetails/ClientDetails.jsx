51098ee1dd50d14ff6516cd3f4900b2c8536d9b7
Will Torres <w.torresjr@gmail.com>
2023-12-16 15:20:08 -0500

    creating thunk to get client details

--- react-vite/src/components/ClientDetails/ClientDetails.jsx
+++ react-vite/src/components/ClientDetails/ClientDetails.jsx
@@ -1,8 +1,21 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useParams } from "react-router-dom";
+
 const ClientDetails = () => {
+  const dispatch = useDispatch();
+  const client_id = useParams("client_id");
+
+  useEffect(() => {
+    if (client_id) {
+      console.log(client_id, "<-------------------- Client ID Found");
+    }
+  }, [dispatch]);
+
   return (
-    <>
-      <h1>Client Details</h1>
-    </>
+    <div className="mainDisplayContain">
+      <h1>{/* {client?.last_name} {client?.first_name} */}</h1>
+    </div>
   );
 };
 
