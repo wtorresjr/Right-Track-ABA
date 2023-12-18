5da2acdbfede519c4e018e010aaf7a107bd607b2
Will Torres <w.torresjr@gmail.com>
2023-12-18 01:38:40 -0500

    Added navigate to create client page

--- react-vite/src/components/ManageClients/ManageClients.jsx
+++ react-vite/src/components/ManageClients/ManageClients.jsx
@@ -1,10 +1,12 @@
 import { useEffect } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { getClientsThunk } from "../../redux/clients";
 import ClientInfo from "./ClientInfo";
 import "./manage-clients.css";
 
 const ManageClients = () => {
+  const navigate = useNavigate();
   const clients = useSelector((state) => state?.clients?.clients?.Clients);
   const dispatch = useDispatch();
 
@@ -16,7 +18,9 @@ const ManageClients = () => {
     <div className="mainDisplayContain">
       <div className="manageClientsHeader">
         <h1>Manage Clients</h1>
-        <button id="addNewClientBtn">Add New Client</button>
+        <button id="addNewClientBtn" onClick={() => navigate("/create-client")}>
+          Add New Client
+        </button>
         <input
           type="text"
           placeholder="Search For A Client"
