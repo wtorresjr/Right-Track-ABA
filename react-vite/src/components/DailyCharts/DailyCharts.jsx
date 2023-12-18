17cad4f9f74a91654d6af1a14eb3210b15f7692e
Will Torres <w.torresjr@gmail.com>
2023-12-17 20:33:26 -0500

    Created daily chart component with link to detail on interval data and the daily chart

--- react-vite/src/components/DailyCharts/DailyCharts.jsx
+++ react-vite/src/components/DailyCharts/DailyCharts.jsx
@@ -1,7 +1,22 @@
+import { NavLink } from "react-router-dom";
+import "./daily-chart.css";
 const DailyCharts = ({ clientCharts }) => {
   return (
     <>
       <h1>Daily Charts</h1>
+      {clientCharts &&
+        clientCharts?.Daily_Charts.map((dt) => {
+          return (
+            <div key={dt?.id} className="clientDCdata">
+              <NavLink to={`/daily-chart/${dt.id}`} className="navLinkStyleDC">
+                <div className="dcButtons">
+                  <div>{dt?.chart_date}</div>
+                  View Intervals
+                </div>
+              </NavLink>
+            </div>
+          );
+        })}
     </>
   );
 };
