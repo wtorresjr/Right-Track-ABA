49a3887d73be9b97e24893716fdb099d665b57ff
Will Torres <w.torresjr@gmail.com>
2023-12-17 21:04:02 -0500

    Started client details - dt component display

--- react-vite/src/components/DiscreetTrials/DiscreetTrials.jsx
+++ react-vite/src/components/DiscreetTrials/DiscreetTrials.jsx
@@ -2,7 +2,10 @@ const DiscreetTrials = ({ clientDT }) => {
   return (
     <>
       <h1>Discreet Trials</h1>
-      {clientDT.first_name}
+      {clientDT &&
+        clientDT?.Discreet_Trials?.map((dt) => {
+          return <div key={dt.id}>{dt?.trial_date}</div>;
+        })}
     </>
   );
 };
