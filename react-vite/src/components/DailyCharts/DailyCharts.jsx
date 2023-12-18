97367df84e758f488f49a08554972d3eeb0c6e5f
Will Torres <w.torresjr@gmail.com>
2023-12-18 01:03:43 -0500

    Added color code to daily perf charts in client detail page

--- react-vite/src/components/DailyCharts/DailyCharts.jsx
+++ react-vite/src/components/DailyCharts/DailyCharts.jsx
@@ -1,15 +1,30 @@
 import { NavLink } from "react-router-dom";
 import "./daily-chart.css";
 const DailyCharts = ({ clientCharts }) => {
+  let dayColorRating;
+  let textColor;
   return (
     <>
-      <h1>Daily Charts</h1>
+      <h1>Daily Performance Charts</h1>
       {clientCharts &&
         clientCharts?.Daily_Charts.map((dc) => {
+          dayColorRating =
+            parseFloat(dc?.avgForChart) >= 1.5
+              ? "green"
+              : parseFloat(dc?.avgForChart) > 1
+              ? "yellow"
+              : parseFloat(dc?.avgForChart) <= 1
+              ? "red"
+              : "black";
           return (
             <div key={dc?.id} className="clientDCdata">
               <NavLink to={`/daily-chart/${dc.id}`} className="navLinkStyleDC">
-                <div className="dcButtons">
+                <div
+                  className="dcButtons"
+                  style={{
+                    border:`5px solid ${dayColorRating}`,
+                  }}
+                >
                   <p>{dc?.chart_date}</p>
                   <div>Days Rating: {dc?.avgForChart}</div>
                   <div>Total Intervals: {dc?.interval_count}</div>
