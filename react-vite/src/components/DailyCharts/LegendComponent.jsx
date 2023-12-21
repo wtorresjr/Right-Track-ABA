d710cf568a6000cfc4db2feb7c51dffe8a8efc79
Will Torres <w.torresjr@gmail.com>
2023-12-20 23:13:59 -0500

    Updated daily chart color coding logic, added behavior legend to client detail view

---
+++ react-vite/src/components/DailyCharts/LegendComponent.jsx
@@ -0,0 +1,22 @@
+import "./daily-chart.css";
+
+const LegendComponent = () => {
+  return (
+    <div id="legend">
+      <h3>Behavior Legend</h3>
+      <div id="colors">
+        <div className="color" style={{ backgroundColor: "red" }}>
+          Poor
+        </div>
+        <div className="color" style={{ backgroundColor: "Orange" }}></div>
+        <div className="color" style={{ backgroundColor: "yellow" }}></div>
+        <div className="color" style={{ backgroundColor: "yellowgreen" }}></div>
+        <div className="color" style={{ backgroundColor: "green" }}>
+          Great
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LegendComponent;
