c33eb75dd68093ec23f7aa8a6528be1d3931166f
Will Torres <w.torresjr@gmail.com>
2023-12-24 21:08:15 -0500

    Working add interval thunk, needs to refresh page on new addition

--- react-vite/src/redux/charts.js
+++ react-vite/src/redux/charts.js
@@ -1,4 +1,12 @@
 const GET_CHART = "charts/getChart";
+const ADD_INTERVAL = "charts/addInterval";
+
+const addInterval = (intervalToAdd) => {
+  return {
+    type: ADD_INTERVAL,
+    payload: intervalToAdd,
+  };
+};
 
 const getChart = (foundChart) => {
   return {
@@ -18,15 +26,33 @@ export const getChartByIdThunk = (chart_id) => async (dispatch) => {
   }
 };
 
+export const addIntervalToChart = (userIntervalInput) => async (dispatch) => {
+  const response = await fetch(`/api/interval/`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(userIntervalInput),
+  });
+  if (response.ok) {
+    const newInterval = await response.json();
+    dispatch(addInterval(newInterval));
+    return newInterval;
+  } else {
+    throw new Error("Error creating new interval");
+  }
+};
+
 const initialState = { chart: null };
 
 function chartsReducer(state = initialState, action) {
   switch (action.type) {
     case GET_CHART:
       return { ...state, chart: action.payload };
+    case ADD_INTERVAL:
+      return { ...state, chart: action.payload };
     default:
       return state;
   }
 }
+
 
 export default chartsReducer;
