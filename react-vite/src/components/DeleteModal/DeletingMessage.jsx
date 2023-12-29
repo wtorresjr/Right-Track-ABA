0fffc730f7d177c0aa353e7b12393178c0a6a03a
Will Torres <w.torresjr@gmail.com>
2023-12-29 00:12:00 -0500

    Added auto close on success delete message

--- react-vite/src/components/DeleteModal/DeletingMessage.jsx
+++ react-vite/src/components/DeleteModal/DeletingMessage.jsx
@@ -1,9 +1,23 @@
-// import { useModal } from "../../context/Modal";
+import { useEffect } from "react";
+import { useModal } from "../../context/Modal";
 import "./delete-modal.css";
 const DeleteMessage = () => {
+  const { closeModal } = useModal();
+
+  //Add timer to close delete message
+  // useEffect(() => {
+
+  // },[dispatch])
+
+  const closeTheMessage = () => {
+    closeModal();
+  };
+
+  setTimeout(closeTheMessage, 1000);
+
   return (
     <div className="deleteMessageContain">
-      <h1>Successfully Deleted...</h1>
+      <h1>Successfully Deleted</h1>
     </div>
   );
 };
