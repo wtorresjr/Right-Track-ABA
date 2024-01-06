d52463dd24f6f9cb709750c003a09e7e5ac5eb43
Will Torres <w.torresjr@gmail.com>
2024-01-06 16:11:26 -0500

    latest style changes

--- react-vite/src/components/ClientListComponent/ChartSlimComponent.jsx
+++ react-vite/src/components/ClientListComponent/ChartSlimComponent.jsx
@@ -3,7 +3,14 @@ import "./chart-slim.css";
 const ChartSlimComponent = ({ chart }) => {
   return (
     <div className="slimDivContain">
-      <div className="slimDiv">{chart.avgForChart}</div>
+      <div className="slimDiv">
+        <label>Chart Date:</label>
+        {chart.chart_date}
+      </div>
+      <div className="slimDiv">
+        <label>Chart Avg:</label>
+        {chart.avgForChart}
+      </div>
     </div>
   );
 };
