e2c394a43c83c2b5a55e767f575ce66b87ab59fb
Will Torres <w.torresjr@gmail.com>
2024-01-06 14:58:14 -0500

    Styled and added component for daily charts list view

--- react-vite/src/components/DailyChartAll/DailyChartAll.jsx
+++ react-vite/src/components/DailyChartAll/DailyChartAll.jsx
@@ -1,44 +1,12 @@
-import { useDispatch, useSelector } from "react-redux";
-import { useEffect, useState } from "react";
-import { getAllChartsThunk } from "../../redux/charts";
 import "./daily-chart-all.css";
+import { ClientListComponent } from "../ClientListComponent";
 
 const DailyChartAll = () => {
-  const dispatch = useDispatch();
-
-  const [loaded, setLoaded] = useState(false);
-  const [message, setMessage] = useState("Loading...");
-  const therapistId = useSelector((state) => state?.session?.user?.id);
-  const allCharts = useSelector((state) => state?.chart?.allCharts);
-
-  useEffect(() => {
-    setLoaded(false);
-    let data = "";
-    const getData = async () => {
-      if (therapistId) {
-        try {
-          const data = await dispatch(getAllChartsThunk(therapistId));
-          console.log(data);
-          if (data?.ok) {
-            setLoaded(true);
-          }
-          if (data?.payload?.message) {
-            setMessage(data?.payload?.message);
-            setLoaded(false);
-          }
-        } catch (error) {
-          setMessage(data?.payload?.message);
-          setLoaded(false);
-        }
-      }
-    };
-    getData();
-  }, [dispatch, message, therapistId, loaded]);
-
   return (
     <div className="mainDisplayContain">
-      <h2>Daily Chart All</h2>
-      {loaded ? therapistId : "...loading"}
+      <h1>Daily Charts Viewer</h1>
+
+      <ClientListComponent />
     </div>
   );
 };
