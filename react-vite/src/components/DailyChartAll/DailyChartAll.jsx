3260da7952a3502ca496592c8d1b508b2f938353
Will Torres <w.torresjr@gmail.com>
2024-01-05 23:03:39 -0500

    Created all dailiy charts component

---
+++ react-vite/src/components/DailyChartAll/DailyChartAll.jsx
@@ -0,0 +1,11 @@
+import "./daily-chart-all.css";
+
+const DailyChartAll = () => {
+  return (
+    <div className="mainDisplayContain">
+      <h2>Daily Chart All</h2>
+    </div>
+  );
+};
+
+export default DailyChartAll;
