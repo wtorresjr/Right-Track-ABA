fbc3332b3c19fc96c6764696fb584b5c349b003c
Will Torres <w.torresjr@gmail.com>
2024-01-07 03:19:57 -0500

    created chart options panel

--- react-vite/src/components/ClientListComponent/ClientListComponent.jsx
+++ react-vite/src/components/ClientListComponent/ClientListComponent.jsx
@@ -2,13 +2,17 @@ import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getClientsThunk, getClientByIDThunk } from "../../redux/clients";
 import "../CreateDailyChart/create-daily-chart.css";
-import ChartComponent from "../ChartComponent/ChartComponent";
+import GraphComponent from "../GraphComponent/GraphComponent";
 
 const ClientListComponent = () => {
   const dispatch = useDispatch();
   const [selectedClient, setSelectedClient] = useState(null);
+  const [selectedChartType, setSelectedChartType] = useState();
+  const [chartDataPoint, setChartDataPoint] = useState();
   const [clientCharts, setClientCharts] = useState();
   const clientList = useSelector((state) => state?.clients?.clients?.Clients);
+  const [startDay, setStartDay] = useState();
+  const [endDay, setEndDay] = useState();
 
   useEffect(() => {
     dispatch(getClientsThunk());
@@ -19,47 +23,88 @@ const ClientListComponent = () => {
       const clientData = await dispatch(getClientByIDThunk(selectedClient));
       if (clientData.ok) {
         setClientCharts(clientData.payload);
-        console.log(clientData.payload);
       }
     };
     findClientCharts(selectedClient);
-  }, [dispatch, selectedClient]);
+  }, [selectedClient]);
 
   return (
-    <div>
-      <div className="newChartMenu">
-        <select
-          id="dcClientSelect"
-          onChange={(e) => setSelectedClient(e.target.value)}
-        >
-          <option>View Charts For...</option>
-          {clientList &&
-            clientList.map((client) => {
-              return (
-                <option key={client?.id} value={client?.id}>
-                  {client?.first_name} {client?.last_name} --- DOB:{" "}
-                  {client?.dob}
-                </option>
-              );
-            })}
-        </select>
-      </div>
-      {clientCharts ? (
-        <ChartComponent clientCharts={clientCharts.Daily_Charts} />
+    <div className="chartView">
+      <div className="chartOptions">
+        <label>
+          Clients:
+          <select
+            id="dcClientSelect"
+            onChange={(e) => setSelectedClient(e.target.value)}
+          >
+            <option>View Charts For...</option>
+            {clientList &&
+              clientList.map((client) => {
+                return (
+                  <option key={client?.id} value={client?.id}>
+                    {client?.first_name} {client?.last_name} --- DOB:{" "}
+                    {client?.dob}
+                  </option>
+                );
+              })}
+          </select>
+        </label>
+        <label>
+          Data Points:
+          <select
+            id="dcClientSelect"
+            onChange={(e) => setChartDataPoint(e.target.value)}
+          >
+            <option>Choose Data Points</option>
+            <option value="AVG">Average Chart Rating</option>
+            <option value="PB">Problem Behaviors</option>
+            <option value="BD">Behavior Duration</option>
+            {selectedClient ? "Loaded selected client" : "Not Loaded"}
+          </select>
+        </label>
+
+        {/* <h4>Filter By Dates: </h4> */}
+        <div className="newChartMenu">
+          <div className="dateDivider">
+            <label>
+              Start:
+              <input type="date"></input>
+            </label>
+            <label>
+              End:
+              <input type="date"></input>
+            </label>
+          </div>
+        </div>
+
+        <div className="chartTypeContain">
+          <label>Chart Type:</label>
+          <button onClick={() => setSelectedChartType("Bar")}>Bar</button>
+          <button>Line</button>
+          <button>Scatter</button>
+          <button>Pie</button>
+        </div>
+        {/* 
+        <div className="chartTypeContain">
+          <label>Chart Type:</label>
+          <button>Bar</button>
+          <button>Line</button>
+          <button>Scatter</button>
+          <button>Pie</button>
+        </div> */}
+      </div>{" "}
+      {selectedClient && chartDataPoint === "AVG" ? (
+        <div className="chartDisplayArea">
+          <GraphComponent
+            chartType={selectedChartType}
+            clientCharts={clientCharts.Daily_Charts}
+          />
+        </div>
       ) : (
-        <p className="errorsPtag">Choose A Client</p>
+        <div className="chartDisplayArea">
+          <p className="errorsPtag">Choose a client</p>
+        </div>
       )}
-      {/* {clientCharts ? (
-        clientCharts.Daily_Charts.map((chart) => {
-          return (
-            <div key={chart.id}>
-              <ChartSlimComponent chart={chart} />
-            </div>
-          );
-        })
-      ) : (
-        <p className="errorsPtag">{"No client selected"}</p>
-      )} */}
     </div>
   );
 };
