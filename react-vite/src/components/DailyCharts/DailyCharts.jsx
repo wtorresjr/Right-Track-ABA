bc8377f2dd46cf3d3bfd12169f7afd9d7693d8f3
Will Torres <w.torresjr@gmail.com>
2024-01-12 01:26:57 -0500

    Fixed undefined bug when refreshing on ClientDetails page

--- react-vite/src/components/DailyCharts/DailyCharts.jsx
+++ react-vite/src/components/DailyCharts/DailyCharts.jsx
@@ -1,3 +1,145 @@
+// import { Link, useParams } from "react-router-dom";
+// import "./daily-chart.css";
+// import LegendComponent from "./LegendComponent";
+// import { useModal } from "../../context/Modal";
+// import { DeleteChartModal } from "../DeleteModal";
+// import { CreateDailyChart, UpdateDailyChart } from "../CreateDailyChart";
+// import returnColor from "../helpers/returnColor";
+// import { useEffect, useState } from "react";
+// import { useDispatch } from "react-redux";
+// import { getClientByIDThunk } from "../../redux/clients";
+
+// const DailyCharts = ({ clientCharts }) => {
+//   const dispatch = useDispatch();
+//   const { client_id } = useParams();
+//   const { setModalContent } = useModal();
+//   const [searchFilter, setSearchFilter] = useState("");
+//   const [filteredCharts, setFilteredCharts] = useState([]);
+
+//   useEffect(() => {
+//     setFilteredCharts(clientCharts);
+//   }, []);
+
+//   useEffect(() => {
+//     const dateResults = clientCharts?.Daily_Charts?.filter((charts) =>
+//       Object.values(charts).some(
+//         (value) =>
+//           typeof value === "string" &&
+//           value?.toLowerCase().includes(searchFilter?.toLowerCase())
+//       )
+//     );
+//     // const intervalTagResults = clientCharts?.Daily_Charts?.filter((item) =>
+//     //   Object.values(...item?.intervals).some(
+//     //     (value) =>
+//     //       typeof value === "string" &&
+//     //       value.toLowerCase().includes(searchFilter?.toLowerCase())
+//     //   )
+//     // );
+
+//     if (dateResults) {
+//       setFilteredCharts(dateResults);
+//     }
+//     // if (intervalTagResults) {
+//     //   setFilteredCharts(intervalTagResults);
+//     // }
+//   }, [searchFilter, clientCharts]);
+
+//   const openDeleteModal = (chart) => {
+//     setModalContent(<DeleteChartModal chartInfo={chart} />);
+//   };
+
+//   const openCreateChartModal = () => {
+//     setModalContent(<CreateDailyChart />);
+//   };
+
+//   const openUpdateChartModal = (dc) => {
+//     setModalContent(<UpdateDailyChart dc={dc} />);
+//   };
+
+//   let dayColorRating;
+
+//   return (
+//     <div className="chartsContain">
+//       <h1>
+//         Daily Performance Charts
+//         <button id="createNewChartBtn" onClick={openCreateChartModal}>
+//           Create New Chart
+//         </button>
+//       </h1>
+
+//       <div className="dcHeader">
+//         <LegendComponent />
+//       </div>
+
+//       <input
+//         type="text"
+//         placeholder="Search Daily Charts (By Date)"
+//         value={searchFilter}
+//         onChange={(e) => setSearchFilter(e.target.value)}
+//       />
+//       <div className="chartTotalsContain">
+//         <h2>
+//           Total Charts: {filteredCharts.length}
+//           {searchFilter ? " (Filtered)" : ""}
+//         </h2>
+//       </div>
+//       <div className="chartsContain">
+//         {filteredCharts &&
+//           filteredCharts?.map((dc) => {
+//             let dayColorRating = returnColor(dc?.avgForChart, "float");
+
+//             if (dc) {
+//               dc?.chart_complete === false ? (dayColorRating = "white") : null;
+//             } else {
+//               return null;
+//             }
+
+//             return (
+//               <div key={dc?.id} className="clientDCdata">
+//                 <Link to={`/daily-chart/${dc?.id}`} className="navLinkStyleDC">
+//                   <div
+//                     className="dcButtons"
+//                     style={{
+//                       border: `5px solid ${dayColorRating}`,
+//                     }}
+//                   >
+//                     <div className="folderText">
+//                       <p>
+//                         <label>Date: {dc?.chart_date}</label>
+//                       </p>
+//                       <div>Total Intervals: {dc?.interval_count}</div>
+//                       <div>Avg Rating: {dc?.avgForChart}</div>
+//                       <p>View Chart</p>
+//                     </div>
+//                   </div>
+//                 </Link>
+//                 <div className="chartCrudBtns">
+//                   <button
+//                     onClick={() => {
+//                       openUpdateChartModal(dc);
+//                     }}
+//                   >
+//                     Edit Chart
+//                   </button>
+//                   <button
+//                     onClick={() => {
+//                       openDeleteModal(dc);
+//                     }}
+//                   >
+//                     Delete Chart
+//                   </button>
+//                 </div>
+//               </div>
+//             );
+//           })}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default DailyCharts;
+
+
 import { Link, useParams } from "react-router-dom";
 import "./daily-chart.css";
 import LegendComponent from "./LegendComponent";
@@ -17,44 +159,21 @@ const DailyCharts = ({ clientCharts }) => {
   const [filteredCharts, setFilteredCharts] = useState([]);
 
   useEffect(() => {
-    const checkIfRefreshed = async () => {
-      if (!clientCharts?.length) {
-        console.log("Page refreshed clientCharts does not exist.");
-        console.log("Client ID->", client_id);
-        const getTheCharts = await dispatch(getClientByIDThunk(client_id));
-        if (getTheCharts) {
-          
-        }
-      }
-      if (clientCharts?.length) {
-        console.log("Client Charts populated");
-      }
-    };
-    checkIfRefreshed();
-  }, []);
+    setFilteredCharts(clientCharts);
+  }, [clientCharts]);
 
   useEffect(() => {
     const dateResults = clientCharts?.Daily_Charts?.filter((charts) =>
       Object.values(charts).some(
         (value) =>
           typeof value === "string" &&
-          value.toLowerCase().includes(searchFilter?.toLowerCase())
+          value?.toLowerCase().includes(searchFilter?.toLowerCase())
       )
     );
-    // const intervalTagResults = clientCharts?.Daily_Charts?.filter((item) =>
-    //   Object.values(...item?.intervals).some(
-    //     (value) =>
-    //       typeof value === "string" &&
-    //       value.toLowerCase().includes(searchFilter?.toLowerCase())
-    //   )
-    // );
 
     if (dateResults) {
       setFilteredCharts(dateResults);
     }
-    // if (intervalTagResults) {
-    //   setFilteredCharts(intervalTagResults);
-    // }
   }, [searchFilter, clientCharts]);
 
   const openDeleteModal = (chart) => {
@@ -91,7 +210,6 @@ const DailyCharts = ({ clientCharts }) => {
         onChange={(e) => setSearchFilter(e.target.value)}
       />
       <div className="chartTotalsContain">
-        {/* <h2>Total Charts: {clientCharts.Num_Of_Charts}</h2> */}
         <h2>
           Total Charts: {filteredCharts.length}
           {searchFilter ? " (Filtered)" : ""}
@@ -101,47 +219,53 @@ const DailyCharts = ({ clientCharts }) => {
         {filteredCharts &&
           filteredCharts?.map((dc) => {
             dayColorRating = returnColor(dc?.avgForChart, "float");
-            {
+
+            if (dc) {
               dc?.chart_complete === false ? (dayColorRating = "white") : null;
+
+              return (
+                <div key={dc?.id} className="clientDCdata">
+                  <Link
+                    to={`/daily-chart/${dc?.id}`}
+                    className="navLinkStyleDC"
+                  >
+                    <div
+                      className="dcButtons"
+                      style={{
+                        border: `5px solid ${dayColorRating}`,
+                      }}
+                    >
+                      <div className="folderText">
+                        <p>
+                          <label>Date: {dc?.chart_date}</label>
+                        </p>
+                        <div>Total Intervals: {dc?.interval_count}</div>
+                        <div>Avg Rating: {dc?.avgForChart}</div>
+                        <p>View Chart</p>
+                      </div>
+                    </div>
+                  </Link>
+                  <div className="chartCrudBtns">
+                    <button
+                      onClick={() => {
+                        openUpdateChartModal(dc);
+                      }}
+                    >
+                      Edit Chart
+                    </button>
+                    <button
+                      onClick={() => {
+                        openDeleteModal(dc);
+                      }}
+                    >
+                      Delete Chart
+                    </button>
+                  </div>
+                </div>
+              );
             }
 
-            return (
-              <div key={dc?.id} className="clientDCdata">
-                <Link to={`/daily-chart/${dc?.id}`} className="navLinkStyleDC">
-                  <div
-                    className="dcButtons"
-                    style={{
-                      border: `5px solid ${dayColorRating}`,
-                    }}
-                  >
-                    <div className="folderText">
-                      <p>
-                        <label>Date: {dc?.chart_date}</label>
-                      </p>
-                      <div>Total Intervals: {dc?.interval_count}</div>
-                      <div>Avg Rating: {dc?.avgForChart}</div>
-                      <p>View Chart</p>
-                    </div>
-                  </div>
-                </Link>
-                <div className="chartCrudBtns">
-                  <button
-                    onClick={() => {
-                      openUpdateChartModal(dc);
-                    }}
-                  >
-                    Edit Chart
-                  </button>
-                  <button
-                    onClick={() => {
-                      openDeleteModal(dc);
-                    }}
-                  >
-                    Delete Chart
-                  </button>
-                </div>
-              </div>
-            );
+            return null; // handle the case where dc is undefined
           })}
       </div>
     </div>
