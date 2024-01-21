dece5a686a0c3fc7050c18ce7e528302b4eb10c0
Will Torres <w.torresjr@gmail.com>
2024-01-21 12:43:13 -0500

    Added recharts implementation of chart for easier data access

--- react-vite/src/components/GraphComponent/GraphComponent.jsx
+++ react-vite/src/components/GraphComponent/GraphComponent.jsx
@@ -1,5 +1,14 @@
-import Chart, { Chart as ChartJS } from "chart.js/auto";
-import { Line, Bar, Scatter, Pie } from "react-chartjs-2";
+import {
+  XAxis,
+  YAxis,
+  Tooltip,
+  CartesianGrid,
+  Line,
+  LineChart,
+  BarChart,
+  Bar,
+  ResponsiveContainer,
+} from "recharts";
 import "./graph-component.css";
 import { useEffect, useState } from "react";
 
@@ -13,129 +22,68 @@ const GraphComponent = ({
 
   useEffect(() => {
     if (chartDataPoint === "AVG") {
-      const chartAvgs = clientCharts
-        ?.map((chart) => chart.avgForChart)
-        .reverse();
-      const chartDates = clientCharts
-        ?.map((chart) => chart.chart_date)
-        .reverse();
-      setChartData({ avgs: chartAvgs, dates: chartDates });
+      setChartData(clientCharts);
     }
 
-    if (chartDataPoint === "PB") {
-      const pbChartData = {
-        labels: clientCharts
-          ?.flatMap((chart) =>
-            chart.intervals.map((interval) => interval.activity)
-          )
-          .reverse(),
-        datasets: Object.keys(
-          clientCharts
-            ?.flatMap((chart) =>
-              chart.intervals.map((interval) => interval.interval_tags)
-            )
-            .reduce((acc, tags) => {
-              Object.keys(tags).forEach((tag) => {
-                acc[tag] = true;
-              });
-              return acc;
-            }, {})
-        ).map((tag) => ({
-          label: tag,
-          data: clientCharts
-            ?.flatMap((chart) =>
-              chart.intervals.map((interval) =>
-                interval.interval_tags[tag] !== undefined
-                  ? interval.interval_tags[tag]
-                  : 0
-              )
-            )
-            .reduce((acc, values) => acc.concat(values), []) // Flatten nested arrays
-            .map((value) => ({ y: value })) // Wrap each value in an object to meet Chart.js expectations
-            .reverse(),
-        })),
-      };
+    // if (chartDataPoint === "PB") {
+    //   const pbChartData = {
+    //     labels: clientCharts
+    //       ?.map((chart) => chart.intervals.map((interval) => interval.activity))
+    //       .reverse(),
+    //     datasets: clientCharts
+    //       ?.map((chart) =>
+    //         chart.intervals.map((interval) => interval.interval_rating)
+    //       )
+    //       .reverse(),
+    //   };
 
-      setChartData(pbChartData);
-    }
+    //   setChartData(pbChartData);
+    // }
 
-    if (chartDataPoint === "BD") {
-    }
+    // if (chartDataPoint === "BD") {
+    // }
   }, [chartDataPoint, selectedClient, clientCharts]);
 
-  const options = {
-    scales: {
-      y: {
-        min: 0,
-        max: 5,
-        grid: {
-          color: "grey",
-        },
-      },
-      x: {
-        grid: {
-          color: "grey",
-        },
-      },
-    },
-  };
+  if (chartData) {
+    console.log(chartData, "<-----Chart Data for PB");
+  }
 
   let ChartComponent;
+  let ChartElement;
+  let fillType;
 
   switch (chartType) {
     case "Line":
-      ChartComponent = Line;
+      ChartComponent = LineChart;
+      ChartElement = Line;
+      fillType = "white";
       break;
     case "Bar":
-      ChartComponent = Bar;
-      break;
-    case "Scatter":
-      ChartComponent = Scatter;
-      break;
-    case "Pie":
-      ChartComponent = Pie;
+      ChartComponent = BarChart;
+      ChartElement = Bar;
+      fillType = "blue";
       break;
     default:
-      ChartComponent = Line;
+      ChartComponent = LineChart;
+      ChartElement = Line;
+      fillType = "white";
   }
 
   return (
     <div className="chartContain">
-      {chartDataPoint === "AVG" ? (
-        <ChartComponent
-          data={{
-            labels: chartData["dates"],
-            datasets: [
-              {
-                label: "Chart Avg",
-                data: chartData["avgs"],
-              },
-            ],
-          }}
-          options={options}
-        />
-      ) : (
-        ""
-      )}{" "}
-      {chartDataPoint === "PB" ? (
-        <ChartComponent
-          data={{
-            labels: chartData["activity"],
-            // ?.map((chart) => chart.activity).reverse(),
-            datasets: [
-              {
-                label: "Interval Avg",
-                data: chartData["interval_tags"],
-                // ?.map((interval) => interval.interval_tags)
-                // .reverse(),
-              },
-            ],
-          }}
-          options={options}
-        />
-      ) : (
-        ""
-      )}
+      <ResponsiveContainer width="100%" height={400}>
+        <ChartComponent data={chartData}>
+          <ChartElement
+            // type="monotone"
+            fill={fillType}
+            dataKey="avgForChart"
+          />
+          <CartesianGrid stroke="#ccc" strokeDasharray={(2, 2)} />
+          <XAxis dataKey="chart_date" reversed={true} />
+          <YAxis domain={[0, 5]} ticks={[0, 1, 2, 3, 4, 5]} />
+          <Tooltip />
+        </ChartComponent>
+      </ResponsiveContainer>
     </div>
   );
 };
