225d8788b5b15de0f67997188ef15d8b08f8f58e
Will Torres <w.torresjr@gmail.com>
2024-02-13 00:16:34 -0500

    added New Discreet Trial button

--- react-vite/src/components/DiscreetTrials/DiscreetTrials.jsx
+++ react-vite/src/components/DiscreetTrials/DiscreetTrials.jsx
@@ -1,17 +1,19 @@
 import { NavLink } from "react-router-dom";
 import "../DailyCharts/daily-chart.css";
 
-
 const DiscreetTrials = ({ clientDT }) => {
   return (
     <div className="chartsContain">
-      <h1>Discreet Trials</h1>
+      <h1>
+        Discreet Trials
+        <button id="createNewChartBtn">New Discreet Trial</button>
+      </h1>
+
       {clientDT &&
         clientDT?.Discreet_Trials?.map((dt) => {
           return (
             <div key={dt.id}>
               <NavLink
-                // onClick={(e) => alert("Feature coming soon...")}
                 to={`/discreet-trial/${dt.id}`}
                 className="navLinkStyleDC"
               >
