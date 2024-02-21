9c7a66e5cb089d5b8ee67fbff9d80d0e81b9fc05
Will Torres <w.torresjr@gmail.com>
2024-02-21 02:23:54 -0500

    Updated seeder data to include letter program

--- app/seeds/seeder_helper_arrays.py
+++ app/seeds/seeder_helper_arrays.py
@@ -78,3 +78,33 @@ shape_choices = [
     "Ellipse",
     "Heart",
 ]
+
+
+letter_choices = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+]
