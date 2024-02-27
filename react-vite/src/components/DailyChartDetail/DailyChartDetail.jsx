a6e7c11f6d7470ca5f781740f78a8a45f6c6c005
Will Torres <w.torresjr@gmail.com>
2024-02-27 01:11:30 -0500

    Commented out unused imports

--- react-vite/src/components/DailyChartDetail/DailyChartDetail.jsx
+++ react-vite/src/components/DailyChartDetail/DailyChartDetail.jsx
@@ -5,7 +5,7 @@ import { useParams, NavLink } from "react-router-dom";
 import { getClientByIDThunk } from "../../redux/clients";
 import "./daily-chart-detail.css";
 import AddIntervalComp from "../AddIntervalComponent/AddIntervalComp";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 import { LegendComponent } from "../DailyCharts";
 import { returnColor } from "../helpers/returnColor";
 import { useModal } from "../../context/Modal";
@@ -15,7 +15,7 @@ import UpdateIntervalComp from "../AddIntervalComponent/UpdateIntervalComp";
 const DailyChartDetail = () => {
   const dispatch = useDispatch();
   const { chart_id } = useParams();
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
   const clientInfo = useSelector((state) => state?.clients?.client_by_id);
   const currentChart = useSelector((state) => state?.chart?.chart?.Chart);
   const currentIntervals = useSelector(
