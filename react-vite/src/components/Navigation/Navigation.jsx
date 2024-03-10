e94ba64f935f78385f62e26a86dfea5b9ef4ba24
Will Torres <w.torresjr@gmail.com>
2024-03-09 19:45:01 -0500

    Fixed logo click home ,oversized div

--- react-vite/src/components/Navigation/Navigation.jsx
+++ react-vite/src/components/Navigation/Navigation.jsx
@@ -1,21 +1,27 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import ProfileButton from "./ProfileButton";
 import "./Navigation.css";
 
 function Navigation() {
+  const navigate = useNavigate();
   const sessionUser = useSelector((state) => state.session.user);
+
+  const goHome = () => {
+    navigate("/");
+  };
   return (
     <>
       {sessionUser && (
         <div className="navContainer">
-          <NavLink to="/">
-            <img
-              className="logo-image"
-              src="../right-track-aba-logo.png"
-              alt="Logo"
-            />
-          </NavLink>
+          {/* <NavLink to="/"> */}
+          <img
+            className="logo-image"
+            src="../right-track-aba-logo.png"
+            alt="Logo"
+            onClick={goHome}
+          />
+          {/* </NavLink> */}
           <ProfileButton />
         </div>
       )}
