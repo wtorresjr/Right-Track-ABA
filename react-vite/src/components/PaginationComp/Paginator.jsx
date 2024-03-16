7938b7805fbec9d35acfcaf7381360e4e280b454
Will Torres <w.torresjr@gmail.com>
2024-03-16 01:43:49 -0400

    Added new pagination from mui

--- react-vite/src/components/PaginationComp/Paginator.jsx
+++ react-vite/src/components/PaginationComp/Paginator.jsx
@@ -1,49 +1,44 @@
-import "./paginator.css";
-import Pagination from "react-bootstrap/Pagination";
-import "./bootstrap.css";
+import TablePagination from "@mui/material/TablePagination";
+import { useState, useEffect } from "react";
 
 const Paginator = ({ numOfCharts, perPage, currentPage, handlePageChange }) => {
-  const totalPages = Math.ceil(numOfCharts / perPage);
-  const pageItems = [];
+  const [page, setPage] = useState(currentPage - 1);
+  const [rowsPerPage, setRowsPerPage] = useState(perPage);
 
-  const renderPageItems = () => {
-    const pagesToShow = 5;
-    const halfPagesToShow = Math.floor(pagesToShow / 2);
+  useEffect(() => {
+    setPage(currentPage - 1);
+  }, [currentPage]);
 
-    let startPage = Math.max(currentPage - halfPagesToShow, 1);
-    let endPage = Math.min(startPage + pagesToShow - 1, totalPages);
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+    handlePageChange(newPage + 1, rowsPerPage);
+  };
 
-    if (totalPages - endPage < halfPagesToShow) {
-      startPage = Math.max(endPage - pagesToShow + 1, 1);
-    }
-
-    for (let i = startPage; i <= endPage; i++) {
-      pageItems.push(
-        <Pagination.Item
-          key={i}
-          active={currentPage === i}
-          onClick={() => handlePageChange(i)}
-        >
-          {i}
-        </Pagination.Item>
-      );
-    }
-
-    return pageItems;
+  const handleChangeRowsPerPage = (event) => {
+    const newRowsPerPage = parseInt(event.target.value, 10);
+    setRowsPerPage(newRowsPerPage);
+    setPage(0);
+    handlePageChange(1, newRowsPerPage);
   };
 
   return (
-    <Pagination>
-      <Pagination.First onClick={() => handlePageChange(1)} />
-      <Pagination.Prev
-        onClick={() => handlePageChange(Math.max(currentPage - 1, 1))}
-      />
-      {renderPageItems()}
-      <Pagination.Next
-        onClick={() => handlePageChange(Math.min(currentPage + 1, totalPages))}
-      />
-      <Pagination.Last onClick={() => handlePageChange(totalPages)} />
-    </Pagination>
+    <TablePagination
+      sx={{
+        backgroundColor: "grey",
+        display: "flex",
+        justifyContent: "center",
+      }}
+      component="div"
+      count={numOfCharts || 0}
+      page={page}
+      onPageChange={handleChangePage}
+      rowsPerPage={rowsPerPage}
+      onRowsPerPageChange={handleChangeRowsPerPage}
+      rowsPerPageOptions={[1, 3, 5, 10, 25, 50, 100]}
+      style={{ color: "white" }}
+      // showLastButton={true}
+      // showFirstButton={true}
+    />
   );
 };
 
