34e4c5e8f4f923e60543dbfe8257047bcc50769e
Will Torres <w.torresjr@gmail.com>
2024-03-17 18:14:26 -0400

    Fixed manage clients default pagination bug jumping from 5 to 10

--- react-vite/src/components/PaginationComp/Paginator.jsx
+++ react-vite/src/components/PaginationComp/Paginator.jsx
@@ -3,7 +3,6 @@ import { useState, useEffect } from "react";
 
 const Paginator = ({ numOfCharts, perPage, currentPage, handlePageChange }) => {
   const [page, setPage] = useState(currentPage - 1);
-  const [rowsPerPage, setRowsPerPage] = useState(perPage);
 
   useEffect(() => {
     setPage(currentPage - 1);
@@ -11,12 +10,11 @@ const Paginator = ({ numOfCharts, perPage, currentPage, handlePageChange }) => {
 
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
-    handlePageChange(newPage + 1, rowsPerPage);
+    handlePageChange(newPage + 1, perPage); // Pass perPage from props
   };
 
   const handleChangeRowsPerPage = (event) => {
     const newRowsPerPage = parseInt(event.target.value, 10);
-    setRowsPerPage(newRowsPerPage);
     setPage(0);
     handlePageChange(1, newRowsPerPage);
   };
@@ -25,7 +23,7 @@ const Paginator = ({ numOfCharts, perPage, currentPage, handlePageChange }) => {
     <TablePagination
       sx={{
         backgroundColor: "grey",
-        borderRadius:"10px",
+        borderRadius: "10px",
         display: "flex",
         justifyContent: "center",
         width: "100%",
@@ -34,7 +32,7 @@ const Paginator = ({ numOfCharts, perPage, currentPage, handlePageChange }) => {
       count={numOfCharts || 0}
       page={page}
       onPageChange={handleChangePage}
-      rowsPerPage={rowsPerPage}
+      rowsPerPage={perPage} // Use perPage from props
       onRowsPerPageChange={handleChangeRowsPerPage}
       rowsPerPageOptions={[1, 3, 5, 10, 25, 50, 100]}
       style={{ color: "white" }}
