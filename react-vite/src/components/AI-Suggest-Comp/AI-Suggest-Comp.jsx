048b3213a92b122905dc441c30a8bbf249032bdd
Will Torres <w.torresjr@gmail.com>
2024-04-26 01:02:58 -0400

    Added client list drop down to ai suggest page

--- react-vite/src/components/AI-Suggest-Comp/AI-Suggest-Comp.jsx
+++ react-vite/src/components/AI-Suggest-Comp/AI-Suggest-Comp.jsx
@@ -1,10 +1,34 @@
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getClientsThunk } from "../../redux/clients";
+
 const AI_Suggest_Comp = () => {
+  const dispatch = useDispatch();
+  const clientList = useSelector((state) => state?.clients?.clients?.Clients);
+
+  useEffect(() => {
+    dispatch(getClientsThunk());
+  }, []);
+
   return (
     <div className="mainDisplayContain">
       <div className="manageClientsHeader">
         <h1>AI Suggestions</h1>
       </div>
-      <p></p>
+      <select
+        id="dcClientSelect"
+        onChange={(e) => setSelectedClient(e.target.value)}
+      >
+        <option>Get AI Suggestions For...</option>
+        {clientList &&
+          clientList.map((client) => {
+            return (
+              <option key={client?.id} value={client?.id}>
+                {client?.first_name} {client?.last_name} --- DOB: {client?.dob}
+              </option>
+            );
+          })}
+      </select>
     </div>
   );
 };
