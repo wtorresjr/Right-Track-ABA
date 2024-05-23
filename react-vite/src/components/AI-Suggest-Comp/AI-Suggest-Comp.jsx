d960d9b9e640d438265fa2af7a6934d4b068e5ab
Will Torres <w.torresjr@gmail.com>
2024-05-23 02:39:20 -0400

    Added get records button, created css file for ai-suggest page

--- react-vite/src/components/AI-Suggest-Comp/AI-Suggest-Comp.jsx
+++ react-vite/src/components/AI-Suggest-Comp/AI-Suggest-Comp.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getClientsThunk } from "../../redux/clients";
+import "./index.css";
 
 const AI_Suggest_Comp = () => {
   const dispatch = useDispatch();
@@ -11,29 +12,40 @@ const AI_Suggest_Comp = () => {
     dispatch(getClientsThunk());
   }, []);
 
-  useEffect(() => {
-    console.log("User selected", selectedClient);
-  }, [selectedClient]);
+  const getRecords = () => {
+    console.log(selectedClient, "<---Client INFO");
+  };
 
   return (
     <div className="mainDisplayContain">
       <div className="manageClientsHeader">
         <h1>AI Suggestions</h1>
       </div>
-      <select
-        id="dcClientSelect"
-        onChange={(e) => setSelectedClient(e.target.value)}
-      >
-        <option>Get AI Suggestions For...</option>
-        {clientList &&
-          clientList.map((client) => {
-            return (
-              <option key={client?.id} value={client?.id}>
-                {client?.first_name} {client?.last_name} --- DOB: {client?.dob}
-              </option>
-            );
-          })}
-      </select>
+
+      {/*Drop Down Menu*/}
+
+      <div className="suggestDropDown">
+        <select
+          id="dcClientSelect"
+          onChange={(e) => setSelectedClient(e.target.value)}
+        >
+          <option>Get AI Suggestions For...</option>
+          {clientList &&
+            clientList.map((client) => {
+              return (
+                <option key={client?.id} value={client?.id}>
+                  {client?.first_name} {client?.last_name} --- DOB:{" "}
+                  {client?.dob}
+                </option>
+              );
+            })}
+        </select>
+        {selectedClient && (
+          <button className="recordBtn" onClick={getRecords}>
+            Get Records
+          </button>
+        )}
+      </div>
     </div>
   );
 };
