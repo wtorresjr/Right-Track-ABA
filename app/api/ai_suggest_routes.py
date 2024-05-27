41187d49c414121fe492cad2a9d76e4ff7c521a2
Will Torres <w.torresjr@gmail.com>
2024-05-27 19:41:17 -0400

    Updated ai suggest route to find all intervals for client

--- app/api/ai_suggest_routes.py
+++ app/api/ai_suggest_routes.py
@@ -10,11 +10,20 @@ ai_suggest = Blueprint("ai-suggest", __name__)
 @ai_suggest.route("/<int:client_id>", methods=["GET"])
 @login_required
 def get_clean_records(client_id):
-    client_records = Client.query.filter_by(id=client_id).all()
+    client_records = (
+        Interval.query.join(Daily_Chart)
+        .filter(Daily_Chart.client_id == client_id)
+        .options(joinedload(Interval.chart))
+        .all()
+    )
     
     if not client_records:
         return jsonify({"message":f"No records found."})
     
-    found_records = [record.to_dict() for record in client_records]
+    # found_intervals = [record.to_dict().get("interval_tags") for record in client_records]
     
-    return jsonify(found_records)
+    found_intervals = [record.to_dict() for record in client_records]
+    
+    
+    
+    return jsonify(found_intervals)
