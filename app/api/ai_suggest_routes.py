1186d7e0d2c2af867defc6144cb67c94d5f36dee
Will Torres <w.torresjr@gmail.com>
2024-06-04 16:06:40 -0400

    added hugging face pytorch and api endpoint for checking ai suggestion

--- app/api/ai_suggest_routes.py
+++ app/api/ai_suggest_routes.py
@@ -1,11 +1,16 @@
 from flask import Blueprint, jsonify, request
+from transformers import pipeline
 from flask_login import current_user, login_user, login_required
 from app.models import db
-from app.models import Client,Daily_Chart, Interval
+from app.models import Client, Daily_Chart, Interval
 from sqlalchemy.orm import joinedload
 from datetime import date
 
 ai_suggest = Blueprint("ai-suggest", __name__)
+
+nlp = pipeline("question-answering", model="deepset/minilm-uncased-squad2",
+               trust_remote_code=True)
+
 
 @ai_suggest.route("/<int:client_id>", methods=["GET"])
 @login_required
@@ -16,14 +21,26 @@ def get_clean_records(client_id):
         .options(joinedload(Interval.chart))
         .all()
     )
-    
+
     if not client_records:
-        return jsonify({"message":f"No records found."})
-    
+        return jsonify({"message": f"No records found."})
+
     # found_intervals = [record.to_dict().get("interval_tags") for record in client_records]
-    
+
     found_intervals = [record.to_dict() for record in client_records]
-    
-    
-    
+
     return jsonify(found_intervals)
+
+
+@ai_suggest.route("/ai-feedback", methods=["POST"])
+@login_required
+def suggest_treatment():
+    data = request.json
+    input_data = data
+
+    print(data, "User sent data")
+
+    response = nlp(input_data)
+    ai_suggestion = response
+
+    return jsonify({"Client Suggestions": ai_suggestion})
